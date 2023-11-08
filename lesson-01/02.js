f9d4da18ba167906d3f73de16914d6a3fbae96de
MakeSM <crispers7437@gmail.com>
2023-11-08 18:02:10 +0500

    Complete 3 tasks

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,9 +1,8 @@
 // генерируем рандомное число
 let number1 = Math.floor(Math.random() * 100);
 //проверяем, четное ли число
-	if (number1 % 2 ===0) {
-		console.log(number1 + ' это четное число');
-	}
-	else {
-		console.log(number1 + ' это нечетное число');
-	}
+if (number1 % 2 === 0) {
+    console.log(number1 + " это четное число");
+} else {
+    console.log(number1 + " это нечетное число");
+}
