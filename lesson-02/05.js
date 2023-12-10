42f642171be71b6c859fc6ccc7ba2df7120b44eb
MakeSM <crispers7437@gmail.com>
2023-12-10 22:45:33 +0500

    attempt 1

---
+++ lesson-02/05.js
@@ -0,0 +1,10 @@
+let passport = {
+   name: 'Petr',
+   surname: 'Petrov',
+};
+
+let clonePassport = Object.assign({}, passport);
+clonePassport.name = 'Ivan';
+
+console.log(passport.name);
+console.log(clonePassport.name);
