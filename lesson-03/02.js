2c64db023ceae3f48adadb66c43a2a265386f6ea
MakeSM <crispers7437@gmail.com>
2024-01-20 23:42:53 +0500

    complete exercise 2 in lesson 3

---
+++ lesson-03/02.js
@@ -0,0 +1,9 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+
+const element = "апельсин";
+
+if (words.indexOf(element) !== -1) {
+   console.log("Ура! нашел");
+} else {
+   console.log("Придется поискать в другом магазине…");
+}
